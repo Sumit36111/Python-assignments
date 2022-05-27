70dbcbf45988bbee4ab2fb240f2575046650f98b
Sumit36111 <100935128+Sumit36111@users.noreply.github.com>
2022-05-27 22:35:12 +0530

    Add files via upload

---
+++ assignment 4 output.py
@@ -0,0 +1,40 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Navodit/AppData/Local/Programs/Python/Python310/assignments/assignment 4.py
+Q1
+Enter mark:70
+Grade: B
+Continue?(y/n)y
+Enter mark:80
+Grade: A
+Continue?(y/n)y
+Enter mark:69
+Grade: B
+Continue?(y/n)n
+Q2
+Enter year:3042
+Not a leap year
+Q3
+Question 1:0x9=0
+Right!
+Question 2:5x8=40
+Right!
+Question 3:7x3=20
+Wrong. The correct answer is 21
+Question 4:7x0=9
+Wrong. The correct answer is 0
+Question 5:3x3=9
+Right!
+Question 6:7x2=14
+Right!
+Question 7:1x3=3
+Right!
+Question 8:9x1=9
+Right!
+Question 9:3x8=24
+Right!
+Question 10:7x6=42
+Right!
+Q4
+177
