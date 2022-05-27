5a34244c2e12aefdbea9b99654445c394c927dfa
Sumit36111 <100935128+Sumit36111@users.noreply.github.com>
2022-05-27 22:36:05 +0530

    Update assignment 4 output.py

--- assignment 4 output.py
+++ assignment 4 output.py
@@ -1,7 +1,3 @@
-Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-= RESTART: C:/Users/Navodit/AppData/Local/Programs/Python/Python310/assignments/assignment 4.py
 Q1
 Enter mark:70
 Grade: B
