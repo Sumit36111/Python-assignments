1acd9a149d13dc6c45a8131bd83c498823d5b52e
Sumit36111 <100935128+Sumit36111@users.noreply.github.com>
2022-06-15 23:28:15 +0530

    Add files via upload

---
+++ assignment 5 output.py
@@ -0,0 +1,94 @@
+Q1
+Enter a string(without apostrophes) and it will be reversed:Hello world
+Reversed string: dlrow olleH
+
+Q2
+Enter starting range:4
+Enter ending range:30
+Enter a number for which the entered range will be divisible by:5
+5
+10
+15
+20
+25
+30
+
+Q3
+Enter length of first side of the triangle:6
+Enter length of second side of the triangle:8
+Enter length of third side of the triangle:10
+Area of the trianle is 24.0
+
+Q4
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+Q5
+Enter number of rows:5
+A 
+BC 
+DEF 
+GHIJ 
+KLMNO 
+
+Q6
+Prime numbers in an input range.
+Enter starting of range(>=2):3
+Enter ending of range:37
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+
+Q7
+77
+154
+231
+308
+385
+462
+
+Q8
+Enter integer:6
+Enter integer:3
+Enter integer:4
+Enter integer:4
+Enter integer:0
+Enter integer:-1
+Enter integer:-7
+Enter integer:23
+Enter integer:6
+a) Positive integers: [6, 3, 4, 4, 0, 23, 6]
+b) Negative integers: [-1, -7]
+c) Odd integers: [3, -1, -7, 23]
+d) Even integers: [6, 4, 4, 0, 6]
+e)
+integer:6 ; number of times it occurs: 2
+integer:3 ; number of times it occurs: 1
+integer:4 ; number of times it occurs: 2
+integer:0 ; number of times it occurs: 1
+integer:-1 ; number of times it occurs: 1
+integer:-7 ; number of times it occurs: 1
+integer:23 ; number of times it occurs: 1
+
+Q9
+Enter a list of words(strings):['Hi','there.','How','are','you?']
+word:Hi ; number of times it occurs:1
+word:there. ; number of times it occurs:1
+word:How ; number of times it occurs:1
+word:are ; number of times it occurs:1
+word:you? ; number of times it occurs:1
