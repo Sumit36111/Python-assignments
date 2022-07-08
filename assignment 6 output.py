0d54d4844848b956d2a8979e62df7cf030d5f2eb
Sumit36111 <100935128+Sumit36111@users.noreply.github.com>
2022-07-08 23:39:41 +0530

    Add files via upload

---
+++ assignment 6 output.py
@@ -0,0 +1,42 @@
+Q1
+Enter a number to check if it is perfect:6
+6 is a perfect number
+Q2
+Enter a word, phrase or sequence to see if it is a palindrome:racecar
+It is a palindrome.
+Q3
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+Q4
+Enter a string to check if it is a pangram: The quick brown fox jumps over the lazy dog
+It is a pangram.
+Q5
+Enter a hyphen-separated sequence of words: Hello-there-master
+Hello-master-there
+Q6
+
+Student ID: 21107221
+Student Name: Matthew
+
+Student ID: 21101201
+Student Name: Henry
+
+Student Name: Henry
+Student Class: 12
+Q7
+<__main__.Student object at 0x0000017A3B7D2C50>
+Navodit
+<__main__.Marks object at 0x0000017A3B7D2B90>
+70
+82
+class Student: <class 'type'> class Marks: <class 'type'>
+Q8
+[[-10, 2, 8], [-7, -3, 10]]
+Q9
+True
+False
+True
+
